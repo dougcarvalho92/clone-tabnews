8589e7e8bb4c6d3707cbf67b7bb3f2948ab65a96
Douglas Carvalho <dscarvalho199@gmail.com>
2026-04-17 09:16:25 -0300

    feat: integrate PostgreSQL database connection and update project configuration
    - Added PostgreSQL connection settings to .env file.
    - Implemented database query functionality in infra/database.js.
    - Updated API endpoint in pages/api/v1/status.js to use the new database query.
    - Modified package.json to include pg dependency and adjusted dev script to run Docker.
    - Updated .vscode/settings.json with SQLTools connection settings.
    - Refactored docker-compose configuration in infra/compose.yaml to use env_file for environment variables.

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,7 +1,12 @@
-function status(request, response){
+import database from "../../../infra/database";
+
+async function status(request, response){
+    const result = await database.query("select 1 + 1 as sum;");
+    console.log(result);
   response.status(200).json({
     "chave": "valor"
   });
+
 }
 
 export default status;
