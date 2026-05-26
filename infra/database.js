6d4fd650804c81ee80c40c8017a71ef4984680a8
Douglas Carvalho <dscarvalho199@gmail.com>
2026-05-26 10:02:52 -0300

    makes database.js client.connect() more robust ro errors

--- infra/database.js
+++ infra/database.js
@@ -17,6 +17,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error("Database query error:", error);
+    throw error;
   } finally {
     await client.end();
   }
