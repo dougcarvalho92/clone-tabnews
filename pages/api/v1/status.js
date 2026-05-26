da4264d578112d2eb185950d75d5e24566cfe77f
Douglas Carvalho <dscarvalho199@gmail.com>
2026-05-26 09:32:53 -0300

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,10 +1,32 @@
 import database from "infra/database";
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const dbOpenedVersionResult = await database.query("SHOW server_version;");
 
-async function status(request, response){
-    const result = await database.query("select 1 + 1 as sum;");
-    console.log(result);
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValues =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionResult = await database.query({
+    text: `SELECT count(*) FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],
+  });
+
+  const dbOpenedConnectionResultReserved = parseInt(dbOpenedConnectionResult.rows[0].count);
+
+
   response.status(200).json({
-    "chave": "valor"
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbOpenedVersionResult.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnectionsValues),
+        opened_connections: dbOpenedConnectionResultReserved,
+      }
+    }
   });
 
 }
