3b4306624e990c355626b1e29cd472efb57c4c8a
Douglas Carvalho <dscarvalho199@gmail.com>
2026-08-24 06:15:38 -0300

    fix: migration

--- infra/migrations/1779825390349_first-migrations-test.js
+++ infra/migrations/1779825390349_first-migrations-test.js
@@ -1,18 +1,24 @@
 /**
  * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
  */
-export const shorthands = undefined;
+const shorthands = undefined;
 
 /**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ * @param {() => void | undefined} run
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+const up = pgm => {};
 
 /**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
+ * @param {import('node-pg-migrate').MigrationBuilder} pgm
+ * @param {() => void | undefined} run
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+const down = pgm => {};
+
+module.exports = {
+  shorthands,
+  up,
+  down,
+};
