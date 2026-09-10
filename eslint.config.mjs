c35456df5e5a114f3945850a3fe1dd7867aef2cd
Douglas Carvalho <dscarvalho199@gmail.com>
2026-09-10 09:57:43 -0300

    revert: rollback eslint to previous config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier/flat";
-import nextPlugin from "@next/eslint-plugin-next";
-
-const nextCoreWebVitalsConfig = nextPlugin.configs?.["core-web-vitals"] ?? {};
 
 const eslintConfig = defineConfig([
-  {
-    ...nextCoreWebVitalsConfig,
-    plugins: {
-      ...(nextCoreWebVitalsConfig.plugins ?? {}),
-      "@next/next": nextPlugin,
-    },
-  },
+  ...nextVitals,
   prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
