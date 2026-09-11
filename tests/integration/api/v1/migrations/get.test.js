6065df26ddecad222cc6618c3271c005e762bc69
Douglas Carvalho <dscarvalho199@gmail.com>
2026-09-11 07:44:50 -0300

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -9,12 +9,15 @@ beforeAll(async () => {
 async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
-
-test("GET to /api/v1/migrations should return 200", async () => {
-  // await cleanDatabase();
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
-  const body = await response.json();
-  expect(Array.isArray(body)).toBe(true);
-  expect(body.length).toBeGreaterThan(0);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      // await cleanDatabase();
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+      const body = await response.json();
+      expect(Array.isArray(body)).toBe(true);
+      expect(body.length).toBeGreaterThan(0);
+    });
+  });
 });
