e5fb9f5841afff9de0953a7e59fe31d3b04fd002
Douglas Carvalho <dscarvalho199@gmail.com>
2026-09-11 07:44:50 -0300

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,18 +9,43 @@ beforeAll(async () => {
 async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;");
 }
-
-test("POST to /api/v1/migrations should return 201", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      // Add any required body parameters here
-    }),
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              // Add any required body parameters here
+            }),
+          },
+        );
+        expect(response.status).toBe(201);
+        const body = await response.json();
+        expect(Array.isArray(body)).toBe(true);
+      });
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              // Add any required body parameters here
+            }),
+          },
+        );
+        expect(response.status).toBe(200);
+        const body = await response.json();
+        expect(Array.isArray(body)).toBe(true);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-  const body = await response.json();
-  expect(Array.isArray(body)).toBe(true);
 });
